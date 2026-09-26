8399103169c23b94ef07d7bb4e77a103b3fe05a5
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2026-09-26 22:31:36 +0530

    Delete kubernetes-cost-optimization/terraform-eks-multi-architecture directory

--- kubernetes-cost-optimization/terraform-eks-multi-architecture/main/output.tf
+++
@@ -1,5 +0,0 @@
-output "aws_ec2_eks_multiarchitecture_iam_role_arn_details" {
-  description = "Details for AWS EC2, EKS, VPC, IAM Role ARN"
-  value       = module.aws_eks_multiarchitecture 
-  sensitive   = true
-}
